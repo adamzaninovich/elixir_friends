0b3fd5c98ae91ee89ff46270304e3ab5866edafa
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2015-12-14 11:26:27 -0600

    removes unnecessary test

--- test/controllers/post_controller_test.exs
+++ test/controllers/post_controller_test.exs
@@ -10,11 +10,6 @@ defmodule ElixirFriends.PostControllerTest do
     {:ok, conn: conn}
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, post_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing Tweets"
-  end
-
   test "renders form for new resources", %{conn: conn} do
     conn = get conn, post_path(conn, :new)
     assert html_response(conn, 200) =~ "New post"
